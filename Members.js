b8a41d0285abc3c8467edb46df3a56047c81f4cd
Mayur Lalwani <mayur_lalwani4@ymail.com>
2020-04-02 20:30:09 +0530

    First Commit

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [
+    {
+        id: 1,
+        name: 'Mayur Lalwani',
+        email: 'lalwanimayur10@yahoo.com',
+        status: 'active'
+    },
+
+    {
+        id: 2,
+        name: 'John Doe',
+        email: 'johndoe@yahoo.com',
+        status: 'inactive'
+    },
+
+    {
+        id: 3,
+        name: 'Carry',
+        email: 'carrt@yahoo.com',
+        status: 'active'
+    }
+];
+module.exports = members;
